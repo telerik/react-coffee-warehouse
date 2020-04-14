a52f89b81e5c463fb8aad08fd699f815ea62cc24
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2020-04-14 21:43:29 +0300

    WIP: add initial employees grid

---
+++ src/components/Grid.jsx
@@ -0,0 +1,112 @@
+
+import React from 'react';
+
+import { Grid as KendoGrid, GridColumn, GridColumnMenuSort, GridColumnMenuFilter } from '@progress/kendo-react-grid';
+import { process } from '@progress/kendo-data-query';
+
+export const Column = GridColumn;
+
+export const ColumnMenu = (props) => {
+    return (
+        <div>
+            <GridColumnMenuSort {...props} />
+            <GridColumnMenuFilter {...props} />
+        </div>
+    );
+}
+
+export const Grid = (props) => {
+    const { data, onDataChange, ...others } = props;
+
+    const [take, setTake] = React.useState(10);
+    const [skip, setSkip] = React.useState(0);
+    const [sort, setSort] = React.useState([]);
+    const [group, setGroup] = React.useState([]);
+    const lastSelectedIndexRef = React.useRef(0);
+
+    const dataState = {
+        take,
+        skip,
+        sort,
+        group
+    };
+
+    const onDataStateChange = React.useCallback(
+        (event) => {
+            setTake(event.data.take);
+            setSkip(event.data.skip);
+            setSort(event.data.sort);
+            setGroup(event.data.group)
+        },
+        [setTake, setSkip, setSort, setGroup]
+    );
+
+    const onSelectionChange = React.useCallback(
+        (event) => {
+            let last = lastSelectedIndexRef.current;
+            const updatedData = data.map(dataItem => {
+                return {...dataItem};
+            });
+            const current = data.findIndex(dataItem => dataItem === event.dataItem);
+
+            if (!event.nativeEvent.shiftKey) {
+                lastSelectedIndexRef.current = last = current;
+            }
+
+            if (!event.nativeEvent.ctrlKey) {
+                updatedData.forEach(item => (item.selected = false));
+            }
+            const select = !event.dataItem.selected;
+            for (let i = Math.min(last, current); i <= Math.max(last, current); i++) {
+                updatedData[i].selected = select;
+            }
+
+            onDataChange(updatedData);
+        },
+        [data, onDataChange]
+    );
+
+    const onHeaderSelectionChange = React.useCallback(
+        (event) => {
+            const checked = event.syntheticEvent.target.checked;
+            const updatedData = data.map(item=>{
+                return {
+                    ...item,
+                    selected: checked
+                };
+            });
+
+            onDataChange(updatedData);
+        },
+        [data, onDataChange]
+    );
+
+    return (
+        <KendoGrid
+            {...dataState}
+            {...others}
+            rowHeight={40}
+            pageable
+            sortable
+            groupable
+            selectedField={'selected'}
+
+            data={process(data, dataState)}
+            onDataStateChange={onDataStateChange}
+
+            onSelectionChange={onSelectionChange}
+            onHeaderSelectionChange={onHeaderSelectionChange}
+        >
+            <Column
+                field={'selected'}
+                width={50}
+                title={' '}
+                headerSelectionValue={
+                    data.findIndex(dataItem => dataItem.selected === false) === -1
+                }
+            />
+            {props.children}
+        </KendoGrid>
+    );
+};
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,43 @@
 import * as React from 'react';
 import { ButtonGroup, Button } from '@progress/kendo-react-buttons';
 import { DateRangePicker } from '@progress/kendo-react-dateinputs';
+import { Grid, Column, ColumnMenu } from './../components/Grid';
+import { employees } from './../resources/employees';
+import { employeeImages } from './../resources/employeeImages';
+
+const FullNameCell = (props) => {
+    const customerPhotoStyle = {
+        display: 'inline-block',
+        width: 32,
+        height: 32,
+        borderRadius: '50%',
+        backgroundSize: '32px 35px',
+        backgroundPosition: 'center center',
+        verticalAlign: 'middle',
+        lineHeight: '32px',
+        boxShadow: 'inset 0 0 1px #999, inset 0 0 10px rgba(0,0,0,.2)',
+        marginLeft: '5px',
+        backgroundImage: employeeImages[props.dataItem.img_id + props.dataItem.gender]
+    };
+
+    const customerName = {
+        display: 'inline-block',
+        verticalAlign: 'middle',
+        lineHeight: '32px',
+        paddingLeft: '10px'
+    };
+
+    return (
+        <td>
+            <div style={customerPhotoStyle} />
+            <div style={customerName}>{ props.dataItem.full_name }</div>
+        </td>
+    );
+};
 
 const Dashboard = () => {
+    const [data, setData] = React.useState(employees.map(dataItem => Object.assign({ selected: false }, dataItem)));
+
     return (
         <div id="Dashboard" className="main-content">
             <div className="card-container">
@@ -27,16 +62,34 @@ const Dashboard = () => {
                 <h3 className="card-title">Team Members</h3>
                 <div className="card-buttons">
                     <ButtonGroup>
-                    <Button togglable={true} defaultChecked={true}>
-                        My Team
-                    </Button>
-                    <Button togglable={true}>
-                        All Teams
-                    </Button>
+                        <Button togglable={true} defaultChecked={true}>
+                            My Team
+                        </Button>
+                        <Button togglable={true}>
+                            All Teams
+                        </Button>
                     </ButtonGroup>
                 </div>
                 <span></span>
-                <div className="card-component">GRID HERE:</div>
+                <div className="card-component">
+                    <Grid data={data} style={{ height: 440, maxWidth: 1300 }} onDataChange={data => setData(data)}>
+                        <Column title={'Employee'}>
+                            <Column field={'full_name'} title={'Contact Name'} columnMenu={ColumnMenu} width={220} cell={FullNameCell} />
+                            <Column field={'job_title'} title={'Job Title'} columnMenu={ColumnMenu} width={220} />
+                            <Column field={'country'} title={'Country'} columnMenu={ColumnMenu} width={100} />
+                            <Column field={'is_online'} title={'Status'} columnMenu={ColumnMenu} width={100} />
+                        </Column>
+                        <Column title={'Performance'}>
+                            <Column field={'rating'} title={'Rating'} columnMenu={ColumnMenu} width={110} />
+                            <Column field={'target'} title={'Engagement'} columnMenu={ColumnMenu} width={230} />
+                            <Column field={'budget'} title={'Budget'} columnMenu={ColumnMenu} width={100} />
+                        </Column>
+                        <Column title={'Contacts'}>
+                            <Column field={'phone'} title={'Phone'} columnMenu={ColumnMenu} width={130} />
+                            <Column field={'address'} title={'Address'} columnMenu={ColumnMenu} width={200} />
+                        </Column>
+                    </Grid>
+                </div>
             </div>
         </div>
     );
