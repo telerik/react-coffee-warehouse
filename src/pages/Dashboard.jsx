e24a3059ed5f774aeced8d7a6d167a07069fe7e5
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2020-04-15 12:48:29 +0300

    WIP: add grid column templates

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,21 @@
 
 import * as React from 'react';
+
 import { ButtonGroup, Button } from '@progress/kendo-react-buttons';
 import { DateRangePicker } from '@progress/kendo-react-dateinputs';
+import {
+    Sparkline,
+    ChartValueAxis,
+    ChartValueAxisItem,
+    ChartArea
+} from '@progress/kendo-react-charts';
+import {
+    useInternationalization
+} from '@progress/kendo-react-intl';
+
 import { Grid, Column, ColumnMenu } from './../components/Grid';
 import { employees } from './../resources/employees';
-import { employeeImages } from './../resources/employeeImages';
+import { images } from './../resources/images';
 
 const FullNameCell = (props) => {
     const customerPhotoStyle = {
@@ -18,7 +29,7 @@ const FullNameCell = (props) => {
         lineHeight: '32px',
         boxShadow: 'inset 0 0 1px #999, inset 0 0 10px rgba(0,0,0,.2)',
         marginLeft: '5px',
-        backgroundImage: employeeImages[props.dataItem.img_id + props.dataItem.gender]
+        backgroundImage: images[props.dataItem.img_id + props.dataItem.gender]
     };
 
     const customerName = {
@@ -32,6 +43,105 @@ const FullNameCell = (props) => {
         <td>
             <div style={customerPhotoStyle} />
             <div style={customerName}>{ props.dataItem.full_name }</div>
+        </td>
+    );
+};
+
+const FlagCell = (props) => {
+    return (
+        <td style={{textAlign: 'center'}}>
+            <img
+                src={images[props.dataItem.country]}
+                style={{width: 30}}
+                alt={props.dataItem.country}
+            />
+        </td>
+    );
+};
+
+const RatingCell = (props) => {
+    const MAX_STARS = 5;
+    const rating = props.dataItem.rating;
+
+    return (
+        <td>
+            {
+                [...new Array(MAX_STARS)].map((_, idx) => {
+                    const isActive = rating <= idx;
+                    return (
+                        <span
+                            className={isActive ? 'k-icon k-i-star' : 'k-icon k-i-star-outline'}
+                            style={isActive ? {color: '#ffa600'} : undefined}
+                        />
+                    );
+                })
+            }
+        </td>
+    );
+};
+
+const OnlineCell = (props) => {
+    const badgeStyle = {
+        display: 'inline-block',
+        padding: '.25em .4em',
+        fontSize: '75%',
+        fontWeight: 700,
+        lineHeight: 1,
+        textAlign: 'center',
+        whiteSpace: 'nowrap',
+        verticalAlign: 'baseline',
+        borderRadius: '.25rem'
+    };
+
+    const onlineBadgeStyle = {
+        ...badgeStyle,
+        color: '#fff',
+        backgroundColor: '#28a745'
+    };
+
+    const offlineBadgeStyle = {
+        ...badgeStyle,
+        color: '#fff',
+        backgroundColor: '#dc3545'
+    };
+    return (
+        <td style={{textAlign: 'center'}}>
+            {
+                props.dataItem.is_online === true ?
+                    <span style={onlineBadgeStyle}>Online</span> :
+                    <span style={offlineBadgeStyle}>Offline</span>
+            }
+        </td>
+    );
+};
+
+const EngagementCell = (props) => {
+    return (
+        <td>
+            <Sparkline
+                type={'bar'}
+                data={props.dataItem.target}
+            >
+                <ChartArea opacity={0} width={200} />
+                <ChartValueAxis visible={false} >
+                    <ChartValueAxisItem min={0} max={130} />
+                </ChartValueAxis>
+            </Sparkline>
+        </td>
+    );
+};
+
+const CurrencyCell = (props) => {
+    const redBoldStyle = {
+        color: '#d9534f',
+        fontWeight: 600
+    };
+
+    const intlService = useInternationalization();
+
+    return (
+        <td>
+             <span style={props.dataItem.budget < 0 ? redBoldStyle : undefined}>{ intlService.formatNumber(props.dataItem.budget, 'c') }</span>
         </td>
     );
 };
@@ -76,13 +186,13 @@ const Dashboard = () => {
                         <Column title={'Employee'}>
                             <Column field={'full_name'} title={'Contact Name'} columnMenu={ColumnMenu} width={220} cell={FullNameCell} />
                             <Column field={'job_title'} title={'Job Title'} columnMenu={ColumnMenu} width={220} />
-                            <Column field={'country'} title={'Country'} columnMenu={ColumnMenu} width={100} />
-                            <Column field={'is_online'} title={'Status'} columnMenu={ColumnMenu} width={100} />
+                            <Column field={'country'} title={'Country'} columnMenu={ColumnMenu} width={100} cell={FlagCell} />
+                            <Column field={'is_online'} title={'Status'} columnMenu={ColumnMenu} width={100} cell={OnlineCell} />
                         </Column>
                         <Column title={'Performance'}>
-                            <Column field={'rating'} title={'Rating'} columnMenu={ColumnMenu} width={110} />
-                            <Column field={'target'} title={'Engagement'} columnMenu={ColumnMenu} width={230} />
-                            <Column field={'budget'} title={'Budget'} columnMenu={ColumnMenu} width={100} />
+                            <Column field={'rating'} title={'Rating'} columnMenu={ColumnMenu} width={110} cell={RatingCell} />
+                            <Column field={'target'} title={'Engagement'} columnMenu={ColumnMenu} width={230} cell={EngagementCell} />
+                            <Column field={'budget'} title={'Budget'} columnMenu={ColumnMenu} width={100} cell={CurrencyCell} />
                         </Column>
                         <Column title={'Contacts'}>
                             <Column field={'phone'} title={'Phone'} columnMenu={ColumnMenu} width={130} />
