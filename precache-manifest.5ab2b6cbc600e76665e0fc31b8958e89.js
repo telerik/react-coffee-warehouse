519741ade0b47623312f13c8faab3ce544bc4b7f
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2020-04-24 15:58:56 +0300

    Updates

---
+++ precache-manifest.5ab2b6cbc600e76665e0fc31b8958e89.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37049361c2e35cd3db0a7694c42e1361",
+    "url": "/react-coffee-warehouse/index.html"
+  },
+  {
+    "revision": "07fac6b2db3ee8e30e58",
+    "url": "/react-coffee-warehouse/static/css/main.e6b75ced.chunk.css"
+  },
+  {
+    "revision": "d552af64bdf68e597c99",
+    "url": "/react-coffee-warehouse/static/js/2.8baf995e.chunk.js"
+  },
+  {
+    "revision": "062913c50fbfed6b707a4635ea70f373",
+    "url": "/react-coffee-warehouse/static/js/2.8baf995e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07fac6b2db3ee8e30e58",
+    "url": "/react-coffee-warehouse/static/js/main.7635d201.chunk.js"
+  },
+  {
+    "revision": "25e40b238f391d37597c",
+    "url": "/react-coffee-warehouse/static/js/runtime-main.24b30e5a.js"
+  },
+  {
+    "revision": "902317fdbd909bd41d7d3ce7507480e3",
+    "url": "/react-coffee-warehouse/static/media/dashboard-icon.902317fd.svg"
+  },
+  {
+    "revision": "767e15a2d37aa8d15243fbb47dc7a5ea",
+    "url": "/react-coffee-warehouse/static/media/github-icon.767e15a2.svg"
+  },
+  {
+    "revision": "a38c6bf1bb2051940b6b688e0a740a1e",
+    "url": "/react-coffee-warehouse/static/media/hamburger-icon.a38c6bf1.svg"
+  },
+  {
+    "revision": "f1a95872000855a776e227749be29003",
+    "url": "/react-coffee-warehouse/static/media/header-bg.f1a95872.png"
+  },
+  {
+    "revision": "3dcf0753205aa58b88ae996e09913308",
+    "url": "/react-coffee-warehouse/static/media/info-background.3dcf0753.svg"
+  },
+  {
+    "revision": "ff22489feae9e2f52b6bd83664248b5f",
+    "url": "/react-coffee-warehouse/static/media/info-icon.ff22489f.svg"
+  },
+  {
+    "revision": "1240069c54476e33278a5567e3159c49",
+    "url": "/react-coffee-warehouse/static/media/kendoka.1240069c.png"
+  },
+  {
+    "revision": "1982fdfcb2d5643a2d23708f8089ad34",
+    "url": "/react-coffee-warehouse/static/media/planning-icon.1982fdfc.svg"
+  },
+  {
+    "revision": "aebef4eac1542e1636b3fb59b2a178e6",
+    "url": "/react-coffee-warehouse/static/media/profile-icon.aebef4ea.svg"
+  },
+  {
+    "revision": "5bded05c4648ff9a54505ec152a73618",
+    "url": "/react-coffee-warehouse/static/media/user-avatar.5bded05c.jpg"
+  }
+]);
